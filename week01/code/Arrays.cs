e86e405d9ded64188417751946be6d10d7db2ff8
Kayode <bubykay@gmail.com>
2025-07-05 21:44:35 +0100

    week 1 algorithm

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,4 @@
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +14,15 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // create an array variable to hold the results
+        List<double> results = new();
+
+        for (int i = 1; i <= length; i++)
+        {
+            results.Add(i * number);
+        }
+
+        return [.. results]; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +38,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        int splitIndex = data.Count - amount;
+        List<int> firstSet = data.GetRange(0, splitIndex);
+        List<int> secondSet = data.GetRange(splitIndex, amount);
+        data.Clear();
+        data.AddRange(secondSet);
+        data.AddRange(firstSet);
+
     }
 }
+
+
