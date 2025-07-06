b6867490acd9129ddbc0486226dd6a7f81d8ea9e
Kayode <bubykay@gmail.com>
2025-07-06 07:59:20 +0100

    added comments

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,4 +1,6 @@
 
+using System.Security.Claims;
+
 public static class Arrays
 {
     /// <summary>
@@ -14,15 +16,21 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        // create an array variable to hold the results
-        List<double> results = new();
+        // this function returns the multiples of a number
+        // for example, if multiple of a numer is 3 and the lenght is 2, it return [3, 6]
 
+        // create a list to store the multiples
+        List<double> results = [];
+
+        // loop from 1 to the specified length
         for (int i = 1; i <= length; i++)
         {
+            // for each loop, multiply it by the number and store it the list
             results.Add(i * number);
         }
 
-        return [.. results]; // replace this return statement with your own
+        // convert the list to an array and return it
+        return [.. results];
     }
 
     /// <summary>
@@ -38,10 +46,18 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // determin the index where rotation begins 
         int splitIndex = data.Count - amount;
+
+        // split the list into two sets "first and second"
         List<int> firstSet = data.GetRange(0, splitIndex);
         List<int> secondSet = data.GetRange(splitIndex, amount);
+
+        // clear the old data list
         data.Clear();
+
+        // join first and second lists together with second list rotated to the first
         data.AddRange(secondSet);
         data.AddRange(firstSet);
 
