62176817579a692335b57a2ddf22c4a754c29ff3
Kayode <bubykay@gmail.com>
2025-08-02 01:33:25 +0100

    complete linkedList

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -33,6 +33,20 @@ public class LinkedList : IEnumerable<int>
     public void InsertTail(int value)
     {
         // TODO Problem 1
+        Node newNode = new(value);
+        if (_head == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+
+            _tail!.Next = newNode;
+            newNode.Prev = _tail;
+            _tail = newNode;
+        }
+
     }
 
 
@@ -65,6 +79,18 @@ public class LinkedList : IEnumerable<int>
     public void RemoveTail()
     {
         // TODO Problem 2
+        if (_tail == _head)
+        {
+            _head = null;
+            _tail = null;
+        }
+        else if (_tail is not null)
+        {
+            _tail.Prev!.Next = null;
+            _tail = _tail.Prev;
+        }
+
+
     }
 
     /// <summary>
@@ -109,6 +135,36 @@ public class LinkedList : IEnumerable<int>
     public void Remove(int value)
     {
         // TODO Problem 3
+        Node current = _head;
+        while (current is not null)
+        {
+            if (current.Data == value)
+            {
+                if (_head == current && _tail == current)
+                {
+                    _head = null;
+                    _tail = null;
+                }
+                else if (_head == current)
+                {
+                    _head.Next.Prev = null;
+                    _head = _head.Next;
+                }
+                else if (_tail == current)
+                {
+                    _tail = _tail.Prev;
+                    _tail.Next = null;
+                }
+                else
+                {
+                    current.Prev.Next = current.Next;
+                    current.Next.Prev = current.Prev;
+                }
+
+                return;
+            }
+            current = current.Next;
+        }
     }
 
     /// <summary>
@@ -117,6 +173,15 @@ public class LinkedList : IEnumerable<int>
     public void Replace(int oldValue, int newValue)
     {
         // TODO Problem 4
+        Node current = _head;
+        while (current != null)
+        {
+            if (current.Data == oldValue)
+            {
+                current.Data = newValue;
+            }
+            current = current.Next;
+        }
     }
 
     /// <summary>
@@ -147,7 +212,12 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+        var current = _tail;
+        while (current is not null)
+        {
+            yield return current.Data; // replace this line with the correct yield return statement(s)
+            current = current.Prev;
+        }
     }
 
     public override string ToString()
@@ -168,8 +238,10 @@ public class LinkedList : IEnumerable<int>
     }
 }
 
-public static class IntArrayExtensionMethods {
-    public static string AsString(this IEnumerable array) {
+public static class IntArrayExtensionMethods
+{
+    public static string AsString(this IEnumerable array)
+    {
         return "<IEnumerable>{" + string.Join(", ", array.Cast<int>()) + "}";
     }
 }
